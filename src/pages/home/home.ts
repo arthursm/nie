adf198b2e73e505e1587d10a35e90fabf91b24c8
Arthur <luckyarthur22@gmail.com>
2018-06-07 19:46:45 -0300

    Comandos uteis

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController } from 'ionic-angular'; 
+import { NativeAudio } from '@ionic-native/native-audio';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private nativeAudio: NativeAudio) {
 
   }
+/*
+ionic cordova platform add browser
 
+ionic cordova run browser
+*/
+
+
+ionViewDidEnter(){
+  console.log('AllahuAkbar');
+  this.nativeAudio.preloadSimple('uniqueId1', '../media/300.mp3').then();}
+a(){
+  console.log('Clicado');
+  this.nativeAudio.preloadSimple('12', 'https://firebasestorage.googleapis.com/v0/b/lustrequalquer.appspot.com/o/300.mp3?alt=media&token=7f1c1ad1-f307-47da-afc8-1afa36056178');
+  this.nativeAudio.play('uniqueId1');
+}
 }

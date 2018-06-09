6221a03fe398f0740efcffed370cab70cd52b70a
Arthur <luckyarthur22@gmail.com>
2018-06-09 16:20:34 -0300

    nue:'(

--- src/pages/hiragana/hiragana.ts
+++ src/pages/hiragana/hiragana.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular'; 
 
 /**
  * Generated class for the HiraganaPage page.
@@ -12,13 +12,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-hiragana',
   templateUrl: 'hiragana.html',
-})
-export class HiraganaPage {
-
+}) 
+ 
+export class HiraganaPage { 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }  
+  oi(){
+   document.write('<audio autoplay><source src="https://firebasestorage.googleapis.com/v0/b/sobrodas-43ded.appspot.com/o/alfabeto%2Fa1.mp3?alt=media&token=6c5d3483-4e88-4b8c-80ac-0a137950f15d" type="audio/mpeg"></audio>');
   }
-   ionViewDidLoad() {
-    console.log('ionViewDidLoad HiraganaPage');
-  }
-  
+   items = [{
+      "audio": "https://firebasestorage.googleapis.com/v0/b/sobrodas-43ded.appspot.com/o/alfabeto%2Fa1.mp3?alt=media&token=6c5d3483-4e88-4b8c-80ac-0a137950f15d",
+      "imagem": "あ",
+      "pergunta": "あ",
+      "resposta": "1 - OVO 39 - Queijo 46 - Galinha 63 - Cajuina 40 - Frango" 
+    },{
+      "audio": "Fulano",
+      "imagem": "Rua AaBbCc, N° 0000",
+      "pergunta": "(99) 9-9999-9999",
+      "resposta": "1 - OVO 39 - Queijo 46 - Galinha 63 - Cajuina 40 - Frango"
+    }] 
 }
